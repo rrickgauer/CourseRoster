8eeaa54df2b3f71014c6d253dc55a95e6a525a37
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-01-08 14:37:01 -0600

    add student search input

--- src/student-search.php
+++ src/student-search.php
@@ -14,23 +14,18 @@
   <div class="container">
     <h1 class="custom-font">Student search</h1>
 
-    <!-- Keyword search form -->
-    <form action="student-search.php" method="post">
-      <div class="row">
-        <div class="col-sm-12 col-md-10">
-          <input class="form-control" type="text" name="keyword" placeholder="Keyword" required autofocus>
-        </div>
-
-        <div class="col-sm-12 col-md-1">
-          <button class="btn blue-button" type="submit">Search</button>
-        </div>
+    <div class="input-group input-group-lg">
+      <div class="input-group-prepend">
+        <span class="input-group-text"><i class='bx bx-search'></i></span>
       </div>
-    </form>
+      <input type="text" class="form-control" id="student-search-input" autofocus placeholder="Enter name">
+    </div>
 
-    <br>
 
-    <!-- If the key word is set, print the results -->
-    <?php if (isset($_POST['keyword'])) printStudentSearchResults($_POST['keyword']); ?>
+
+
+
+
 
   </div>
 </body>
