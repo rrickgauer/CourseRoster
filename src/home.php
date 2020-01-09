b01b6e88932ba537c117ab4083168fd7123028d0
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-01-08 22:55:21 -0600

    add home.php

---
+++ src/home.php
@@ -0,0 +1,27 @@
+<?php
+
+include('functions.php');
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <?php include('head.php'); ?>
+    <title></title>
+  </head>
+  <body>
+    <?php include('navbar.php'); ?>
+
+    <div class="container">
+
+      <h1 class="custom-font">Home</h1>
+
+
+
+
+    </div>
+
+  </body>
+</html>
--- src/navbar.php
+++ src/navbar.php
@@ -6,6 +6,8 @@
 
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link" href="home.php">Home</a></li>
+
         <li class="nav-item"><a class="nav-link" href="enrolled.php">Roster</a></li>
         <li class="nav-item"><a class="nav-link" href="course-search.php">Course Search</a></li>
         <li class="nav-item"><a class="nav-link" href="following.php">Following</a></li>
