25608715c777a0d6d0cee7d89b37c9eaa0291c59
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-01-09 04:15:54 -0600

    navbar css

--- src/navbar.php
+++ src/navbar.php
@@ -6,13 +6,13 @@
 
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
-        <li class="nav-item" id="nav-item-home"><a class="nav-link" href="home.php">Course Roster</a></li>
-        <li class="nav-item" id="nav-item-courses"><a class="nav-link" href="course-search.php">Courses</a></li>
-        <li class="nav-item" id="nav-item-students"><a class="nav-link" href="student-search.php">Students</a></li>
+        <li class="nav-item"><a class="nav-link" id="nav-item-home" href="home.php">Course Roster</a></li>
+        <li class="nav-item"><a class="nav-link" id="nav-item-courses" href="course-search.php">Courses</a></li>
+        <li class="nav-item"><a class="nav-link" id="nav-item-students" href="student-search.php">Students</a></li>
 
-        <li class="nav-item dropdown" id="nav-item-more">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">More</a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="nav-item-more" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">More</a>
+          <div class="dropdown-menu" aria-labelledby="nav-item-more">
             <a class="dropdown-item" id="nav-item-settings" href="account-info.php">Settings</a>
             <a class="dropdown-item" id="nav-item-logout" href="logout.php">Logout</a>
           </div>
