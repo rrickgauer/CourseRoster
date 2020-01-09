c006afff28829e061923700687a42302066dce91
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-01-08 23:23:42 -0600

    add summary stats

--- src/home.php
+++ src/home.php
@@ -3,6 +3,8 @@
 include('functions.php');
 session_start();
 
+$student = getStudentInfo($_SESSION['userID'])->fetch(PDO::FETCH_ASSOC);
+
 ?>
 
 <!DOCTYPE html>
@@ -16,7 +18,17 @@ session_start();
 
     <div class="container">
 
-      <h1 class="custom-font">Home</h1>
+      <div id="home-summary">
+        <div class="name d-inline"><?php echo $student['First'] . ' ' . $student['Last']; ?></div>
+        <div class="d-inline float-right"><a href="account-info.php"><i class='bx bx-cog'></i></a></div><br>
+        <div class="d-inline home-count-stat"><?php echo $student['coursesCount']; ?> courses</div>
+        <div class="d-inline home-count-stat"><?php echo $student['followersCount']; ?> followers</div>
+        <div class="d-inline home-count-stat"><?php echo $student['followingCount']; ?> following</div>
+      </div>
+
+      
+
+
 
 
 
