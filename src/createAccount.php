eeeda676b37b98fe143e001a4656a9aa7f00b0b4
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-01-14 06:55:29 -0600

    2 passwords

--- src/createAccount.php
+++ src/createAccount.php
@@ -47,8 +47,9 @@ if (isset($_POST['first']) && isset($_POST['last']) && isset($_POST['email']) &&
       <input class="form-control" type="text" name="first" placeholder="First name" required autofocus><br>
       <input class="form-control" type="text" name="last" placeholder="Last name" required><br>
       <input class="form-control" type="email" name="email" placeholder="Email" required><br>
-      <input class="form-control" type="password" name="password" placeholder="Password" required><br>
-      <input class="btn btn-primary form-control" type="submit" name="createButton" value="Sign up">
+      <input class="form-control" type="password" id="new-password-1-create" name="password" placeholder="Password" required><br>
+      <input class="form-control" type="password" id="new-password-2-create" name="password-2" placeholder="Reenter password" required><br>
+      <input class="btn btn-primary form-control" id="save-password-btn" type="submit" name="createButton" value="Sign up" disabled>
     </form>
 
     <br>
@@ -60,6 +61,41 @@ if (isset($_POST['first']) && isset($_POST['last']) && isset($_POST['email']) &&
 
 
   </div>
+
+  <script>
+    $(document).ready(function() {
+      $("#new-password-1-create, #new-password-2-create").on("keyup", validatNewPassword);
+    });
+
+    function validatNewPassword() {
+      var password1 = $("#new-password-1-create").val();
+      var password2 = $("#new-password-2-create").val();
+
+      if (isNewPasswordsNonEmpty()) {
+        if (areNewPasswordsEqual() == false) {
+          $("#new-password-2-create").addClass("invalid");
+          $(".password-status").html("<i class='bx bxs-error'></i>");
+          $("#save-password-btn").prop("disabled", true);
+
+        } else {
+          $("#new-password-2-create").removeClass("invalid");
+          $(".password-status").html("<i class='bx bxs-check-circle'></i>");
+          $("#save-password-btn").prop("disabled", false);
+        }
+      }
+
+    }
+
+    function areNewPasswordsEqual() {
+      return ($("#new-password-1-create").val() == $("#new-password-2-create").val());
+    }
+
+    function isNewPasswordsNonEmpty() {
+      var password1 = $("#new-password-1-create").val();
+      var password2 = $("#new-password-2-create").val();
+      return (password1.length > 0 && password2.length > 0)
+    }
+  </script>
 </body>
 
 </html>
