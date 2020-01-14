cff36f9f29bc702cabf3535e372fd4fbcce93716
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-01-14 05:52:12 -0600

    formatting

--- src/test.php
+++ src/test.php
@@ -11,34 +11,11 @@
 
 <body>
   <?php include('navbar.php'); ?>
-
   <div class="container">
 
     <h1 class="custom-font text-center blue-font">Test</h1>
 
-
-    <?php
-        $hash = password_hash("901hayrack", PASSWORD_DEFAULT);
-
-        if (password_verify('901hayrack', $hash)) {
-          echo 'Password is valid!';
-        } else {
-          echo 'Invalid password.';
-        }
-    ?>
-
-
-
-
-
-
-
-
-
-
   </div>
-
-
 </body>
 
 </html>
