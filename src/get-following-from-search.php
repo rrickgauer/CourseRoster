4d11c1a8d01284cd8d491a4c4c3331079e84e0d8
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-01-15 22:01:53 -0600

    update function call

--- src/get-following-from-search.php
+++ src/get-following-from-search.php
@@ -1,8 +1,6 @@
 <?php
-
 include_once('functions.php');
-
-$following = getStudentFollowingByQuery($_GET['studentID'], $_GET['query']);
+$following = getStudentFollowing($_GET['studentID'], $_GET['query']);
 
 if (isset($_GET['view'])) {
   if ($_GET['view'] == 'table') {
@@ -14,8 +12,4 @@ if (isset($_GET['view'])) {
 }
 
 exit;
-
-
-
-
 ?>
