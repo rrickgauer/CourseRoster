0b272f83c57bdfb8898a4bd4ef6ed225d5c41811
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-01-16 06:52:27 -0600

    remove files not needed

--- src/unfollow.php
+++
@@ -1,7 +0,0 @@
-<?php
-  session_start();
-  include('functions.php');
-  delete_Student_Followers($_GET['studentID'], $_SESSION['userID']);
-  $studentID = $_GET['studentID'];
-  header("Location: student.php?studentID=$studentID");
-?>
