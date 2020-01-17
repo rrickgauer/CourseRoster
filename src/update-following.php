324f79447c796d3d61a99006a6ea2bda56bac063
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-01-16 19:14:51 -0600

    added insertActivity functions

--- src/update-following.php
+++ src/update-following.php
@@ -4,9 +4,11 @@ session_start();
 
 if (isFollowing($_GET['studentID'], $_SESSION['userID'])) {
   removeStudentFollower($_GET['studentID'], $_SESSION['userID']);
+  insertActivity($_SESSION['userID'], $_GET['studentID'], 'unfollowed');
   echo 'Follow';
 } else {
   insertStudentFollower($_GET['studentID'], $_SESSION['userID']);
+  insertActivity($_SESSION['userID'], $_GET['studentID'], 'followed');
   echo 'Following';
 }
 
