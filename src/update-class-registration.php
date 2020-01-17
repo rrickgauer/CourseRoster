e4ec155475c214d33793a46ff81ed4256edf9b0f
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-01-16 19:38:36 -0600

    fixed error

--- src/update-class-registration.php
+++ src/update-class-registration.php
@@ -4,8 +4,11 @@ include_once('functions.php');
 
 if (isStudentEnrolled($_SESSION['userID'], $_GET['classID'])) {
   dropEnrolledCourse($_SESSION['userID'], $_GET['classID']);
+  insertActivity($_SESSION['userID'], $_GET['classID'], 'dropped');
 } else {
   enrollStudentInCourse($_SESSION['userID'], $_GET['classID']);
+  insertActivity($_SESSION['userID'], $_GET['classID'], 'enrolled');
+
 }
 
 $class = getCourseInformation($_GET['classID'])->fetch(PDO::FETCH_ASSOC);
