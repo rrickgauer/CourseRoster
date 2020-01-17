f94f0046f1244383d10929bc777ec362cd5f405a
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-01-16 19:14:13 -0600

    update

---
+++ src/test.php
@@ -0,0 +1,34 @@
+<?php
+include('functions.php');
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <?php include('head.php'); ?>
+    <title>Test</title>
+  </head>
+  <body>
+    <?php include('navbar.php'); ?>
+
+    <div class="container">
+
+      <h1 class="custom-font text-center">Test</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+  </body>
+</html>
